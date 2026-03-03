28a5a381a5d84685b11825f2ff299878795f1755
SkullBullet333 <rrochapablo@gmail.com>
2026-03-03 16:29:30 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
+const CACHE_NAME = "financeiro-v1";
+
+const urlsToCache = [
+  "/meu-app-pwa/",
+  "/meu-app-pwa/index.html",
+  "/meu-app-pwa/manifest.json"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
